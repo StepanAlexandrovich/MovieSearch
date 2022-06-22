661b4e3507e159700586298bb5a0adbdfda0be99
stepan <fisenko.stepan@mail.ru>
2022-06-22 20:42:49 +0300

    lesson_3

--- app/src/main/java/java/android/moviesearch/MainActivity.kt
+++ app/src/main/java/java/android/moviesearch/MainActivity.kt
@@ -1,14 +1,11 @@
-package java.android.moviesearch;
+package java.android.moviesearch
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
