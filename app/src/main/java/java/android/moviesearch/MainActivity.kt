ad8c18e41d707650bdc083846859ce309ee44d76
stepan <fisenko.stepan@mail.ru>
2022-06-23 20:20:05 +0300

    lesson_1_4

--- app/src/main/java/java/android/moviesearch/MainActivity.kt
+++ app/src/main/java/java/android/moviesearch/MainActivity.kt
@@ -2,10 +2,28 @@ package java.android.moviesearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<AppCompatButton>(R.id.buttonUp).setOnClickListener(this)
+        findViewById<AppCompatButton>(R.id.buttonDown).setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+
+        when (p0?.getId()) {
+            R.id.buttonUp -> message("UP")
+            R.id.buttonDown -> message("DOWN")
+        }
+    }
+
+    private fun message(text:String){
+        Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
     }
 }
